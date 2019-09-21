1fb3f3058aa7057ac94be36151604bfa89734daa
jenbeltran <francescajenbeltran@gmail.com>
2019-09-21 16:45:13 -0400

    update orgs json files

--- src/tdBankData/organizations.js
+++ src/tdBankData/organizations.js
@@ -4,23 +4,26 @@ const rawData2 = require('./dataSetOrg3.json');
 
 const OrganizationInfo = [
 	{
-		name        : 'CircleTO',
-		description : 'Dedicated to improve the life of the underhoused because our community is a circle.',
+		name        : 'Fred Victor',
+		description :
+			'Fred Victor is a social service charitable organization that fosters long-lasting and positive change in the lives of homeless and low-income people living across Toronto.',
+		imageURL    : 'https://www.fredvictor.org/wp-content/uploads/2019/09/IMG_0029-e1569007745195.jpg',
 		projects    : [
 			{
-				name      : 'Coffee For Everyone',
+				name      : 'Daily Living',
 				accountID : 'ac7619df-ab70-40b5-abbb-8801bb55e9e3',
 				getTxs    : () => {
 					return getTx('Food and Dining', rawData0);
 				}
 			}
 		],
-		lat         : '43.661233',
-		lon         : '-79.372162'
+		lat         : '43.653626',
+		lon         : '-79.372972'
 	},
 	{
 		name        : 'Support The Kids',
 		description : 'Dedicated to providing after school activities to children in low income households',
+		imageURL    : 'https://d.newsweek.com/en/full/1130448/children-reading-stock.jpg',
 		projects    : [
 			{
 				name      : 'Lets go shopping!',
@@ -36,6 +39,8 @@ const OrganizationInfo = [
 	{
 		name        : 'Rexdale Community Garden',
 		description : 'We need some money for the rexdale community garden.',
+		imageURL    :
+			'https://www.totallandscapecare.com/wp-content/uploads/sites/5/2017/10/old-lady-garden-terrace.jpg',
 		projects    : [
 			{
 				name      : 'Winter Clean Up',
@@ -72,6 +77,5 @@ function tdBankRoute(req, res, next) {
 }
 
 module.exports = {
-	organizationInfo : getOrganizationInfo,
-	get              : tdBankRoute
+	get : tdBankRoute
 };
--- src/tdBankData/test.js
+++ src/tdBankData/test.js
@@ -1,4 +1,3 @@
-let org = require('./organizations.js');
-
+let org = require('./organizations.js.js.js');
 let OrganizationInfo = org.organizationInfo();
 console.log(OrganizationInfo[2].projects[0].getTxs());
